07fc3047fc27a419e012bb507df0dfad26a1b231
Maudy Nur Avianti <namaaaku.uddy@Yahoo.com>
2016-03-30 11:39:21 +0700

    clone di tempat yang benar di branch coba

---
+++ dao-api/src/main/java/com/ega/dao/MataKuliahDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.ega.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.ega.entities.MataKuliah;
+
+/**
+ *
+ * @author Ega Prianto
+ */
+public interface MataKuliahDao extends CrudRepository<MataKuliah, Integer>{
+}
