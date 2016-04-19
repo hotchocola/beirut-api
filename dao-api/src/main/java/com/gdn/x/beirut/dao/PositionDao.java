863e3ce78d9aa1642e927c171f0537c7a8050f70
Cheria <cheria.dghouls@gmail.com>
2016-04-19 22:51:52 +0700

    Benerin nama doang

--- dao-api/src/main/java/com/gdn/x/beirut/dao/PositionDao.java
+++ dao-api/src/main/java/com/gdn/x/beirut/dao/PositionDao.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gdn.x.beirut.entities.Position;
 
-public interface PositionDao extends JpaRepository<Position, String>{
+public interface PositionDAO extends JpaRepository<Position, String>{
    List<Position> findByTitle(String title);
    List<Position> findByTitleContaining(String title);
 }
