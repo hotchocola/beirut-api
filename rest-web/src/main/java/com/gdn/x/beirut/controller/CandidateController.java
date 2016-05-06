3943ab2d571bec25e1725caeb238945566edbf6d
Ega Prianto <egaprianto85@gmail.com>
2016-05-06 17:32:18 +0700

    benerin get allCandidate(belum di coba)

--- rest-web/src/main/java/com/gdn/x/beirut/controller/CandidateController.java
+++ rest-web/src/main/java/com/gdn/x/beirut/controller/CandidateController.java
@@ -7,6 +7,7 @@ import org.dozer.Mapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,7 +16,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.gdn.common.web.wrapper.response.GdnRestListResponse;
 import com.gdn.common.web.wrapper.response.GdnRestSingleResponse;
 import com.gdn.common.web.wrapper.response.PageMetaData;
+import com.gdn.x.beirut.dto.request.CandidateDTORequest;
 import com.gdn.x.beirut.dto.response.CandidateDTOResponse;
+import com.gdn.x.beirut.dto.response.PositionDTOResponse;
 import com.gdn.x.beirut.entities.Candidate;
 import com.gdn.x.beirut.services.CandidateService;
 import com.wordnik.swagger.annotations.Api;
@@ -64,13 +67,36 @@ public class CandidateController {
         new PageMetaData(50, 0, candidates.size()), requestId);
   }
 
-  @RequestMapping(value = "/api/position/insertNewPosition", method = RequestMethod.POST,
+  @RequestMapping(value = "/api/candidate/getAllCandidate", method = RequestMethod.POST,
       produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
   @ApiOperation(value = "insert new position", notes = "memasukan posisi baru.")
   @ResponseBody
-  public GdnRestListResponse<CandidateDTOResponse> getAllMahasiswaDetailStatus(
-      @RequestParam String clientId, @RequestParam String storeId, @RequestParam String requestId,
-      @RequestParam String channelId, @RequestParam String username) {
-    return null;
+  public GdnRestListResponse<CandidateDTOResponse> getAllCandidate(@RequestParam String clientId,
+      @RequestParam String storeId, @RequestParam String requestId, @RequestParam String channelId,
+      @RequestParam String username) {
+    List<Candidate> candidates = this.candidateService.getAllCandidates();
+    List<CandidateDTOResponse> candidateResponse = new ArrayList<>();
+    for (Candidate candidate : candidates) {
+      CandidateDTOResponse newCandidateDTORes = new CandidateDTOResponse();
+      dozerMapper.map(candidate, newCandidateDTORes);
+      candidateResponse.add(newCandidateDTORes);
+    }
+    return new GdnRestListResponse<>(candidateResponse,
+        new PageMetaData(50, 0, candidateResponse.size()), requestId);
+  }
+
+  @RequestMapping(value = "/api/position/insertNewCandidate", method = RequestMethod.POST,
+      consumes = {MediaType.APPLICATION_JSON_VALUE},
+      produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+  @ApiOperation(value = "insert new position", notes = "memasukan posisi baru.")
+  @ResponseBody
+  public GdnRestSingleResponse<PositionDTOResponse> insertNewPosition(@RequestParam String clientId,
+      @RequestParam String storeId, @RequestParam String requestId, @RequestParam String channelId,
+      @RequestParam String username, @RequestBody CandidateDTORequest candreq) {
+    Candidate temp = new Candidate(storeId);
+    dozerMapper.map(candreq, temp);
+    PositionDTOResponse result = new PositionDTOResponse();
+    dozerMapper.map(temp, result);
+    return new GdnRestSingleResponse<PositionDTOResponse>(result, requestId);
   }
 }
