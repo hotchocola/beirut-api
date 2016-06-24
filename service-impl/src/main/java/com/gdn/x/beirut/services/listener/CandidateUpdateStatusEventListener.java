215818952ae3c04e41f5d0a7cd5ce336b5894ae1
Maudy Nur Avianti <namaaaku.uddy@Yahoo.com>
2016-06-24 12:08:46 +0700

    ganti event listener updatecandidatestatus

--- service-impl/src/main/java/com/gdn/x/beirut/services/listener/CandidateUpdateStatusEventListener.java
+++ service-impl/src/main/java/com/gdn/x/beirut/services/listener/CandidateUpdateStatusEventListener.java
@@ -1,18 +1,17 @@
 package com.gdn.x.beirut.services.listener;
 
-import java.util.UUID;
-
 import javax.annotation.Resource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.solr.core.SolrTemplate;
+import org.springframework.data.solr.core.query.PartialUpdate;
+import org.springframework.data.solr.core.query.SimpleQuery;
+import org.springframework.data.solr.core.query.SimpleStringCriteria;
 import org.springframework.stereotype.Service;
 
 import com.gdn.common.base.domainevent.subscriber.DomainEventListener;
 import com.gdn.common.base.domainevent.subscriber.SubscribeDomainEvent;
-import com.gdn.common.base.mapper.GdnMapper;
 import com.gdn.x.beirut.domain.event.model.CandidateUpdateStatus;
 import com.gdn.x.beirut.domain.event.model.DomainEventName;
 import com.gdn.x.beirut.solr.entities.CandidatePositionSolr;
@@ -28,16 +27,18 @@ public class CandidateUpdateStatusEventListener
   @Resource(name = "xcandidatePositionTemplate")
   private SolrTemplate candidatePositionTemplate;
 
-  @Autowired
-  private GdnMapper gdnMapper;
-
   @Override
   public void onDomainEventConsumed(CandidateUpdateStatus message) throws Exception {
     LOG.info("consuming message from kafka : {}", new Object[] {message});
-    CandidatePositionSolr newCandidateSolr =
-        gdnMapper.deepCopy(message, CandidatePositionSolr.class);
-    newCandidateSolr.setId(UUID.randomUUID().toString());
-    candidatePositionTemplate.saveBean(newCandidateSolr);
-  }
+    CandidatePositionSolr exist = candidatePositionTemplate.queryForObject(
+        new SimpleQuery(new SimpleStringCriteria("idCandidate:" + message.getIdCandidate())),
+        CandidatePositionSolr.class);
+    if (!exist.getIdCandidate().equals(null)) {
+      PartialUpdate update = new PartialUpdate("idPosition", exist.getIdPosition());
+      update.setValueOfField("status", message.getStatus());
+      candidatePositionTemplate.saveBean(update);
+      candidatePositionTemplate.commit();
+    }
 
+  }
 }
